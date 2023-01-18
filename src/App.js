1e0cd60e43a0e00d0e98a2ac384c8f71c18171fb
Ana Delic <milojevican@gmail.com>
2023-01-18 17:05:47 +0100

    Chane App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { saveAs } from 'file-saver';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [meme, setMeme] = useState('doge');
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const url = topText
+    ? `https://api.memegen.link/images/${meme}/${topText}/${bottomText}.jpg`
+    : `https://api.memegen.link/images/${meme}`;
+
+  const downloadImage = () => {
+    let url = `https://api.memegen.link/images/${meme}`;
+    saveAs(url, 'meme');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body className="App">
+      <header>
+        <h1> React Meme Generator </h1>
       </header>
-    </div>
+      <main>
+        <img
+          src={url}
+          className="memeImg"
+          alt="meme"
+          data-test-id="meme-image"
+        />
+        <label htmlFor="memeTemplate">
+          Meme Template:
+          <input
+            name="memeTemplate"
+            htmlFor="memeTemplate"
+            value={meme}
+            onChange={(e) => setMeme(e.currentTarget.value)}
+          />
+        </label>
+        <label htmlFor="topText">
+          Top text
+          <input
+            name="topText"
+            value={topText}
+            onChange={(event) => {
+              setTopText(event.currentTarget.value);
+            }}
+          />
+        </label>
+        <label htmlFor="bottomText">
+          Bottom text
+          <input
+            name="bottomText"
+            value={bottomText}
+            onChange={(event) => {
+              setBottomText(event.currentTarget.value);
+            }}
+          />
+        </label>
+        <button onClick={downloadImage}>Download</button>
+      </main>
+    </body>
   );
 }
-
-export default App;
